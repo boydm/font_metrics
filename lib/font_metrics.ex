a1c3b60815948a7a06dbf8a612ecc76c30d1f21a
Boyd Multerer <boyd@multerer.com>
2019-02-28 16:55:22 +1300

    lots of tests and adding shorten

--- lib/font_metrics.ex
+++ lib/font_metrics.ex
@@ -10,8 +10,6 @@ defmodule FontMetrics do
   
   """
 
-  import IEx
-
   @version            "0.1.0"
   @name               to_string(__MODULE__)
 
@@ -22,7 +20,6 @@ defmodule FontMetrics do
 
   # ===========================================================================
   @derive [{Msgpax.Packer, include_struct_field: true}]
-  @derive Msgpax.Packer
   defstruct version: nil,
   source: nil, direction: nil, smallest_ppem: nil, units_per_em: nil,
   max_box: nil, ascent: nil, descent: nil, metrics: %{}, kerning: %{}
@@ -30,7 +27,7 @@ defmodule FontMetrics do
   # ===========================================================================
   defmodule Error do
     @moduledoc false
-    defexception error: nil, data: nil
+    defexception message: "", error: nil, data: nil
   end
 
 
@@ -71,11 +68,19 @@ defmodule FontMetrics do
 
   #--------------------------------------------------------
   def from_binary( binary ) when is_binary(binary) do
-    :zlib.unzip( binary )
-    |> Msgpax.unpack()
-    |> case do
-      {:ok, map} -> intrepret_unpack(map)
+    with {:ok, bin} <- do_unzip( binary ),
+    {:ok, map} <- Msgpax.unpack( bin ) do
+      intrepret_unpack( map )
+    else
       err -> err
+    end
+  end
+
+  defp do_unzip( binary ) do
+    try do
+      {:ok, :zlib.unzip( binary )}
+    rescue
+      _ -> {:error, :unzip}
     end
   end
 
@@ -125,7 +130,7 @@ defmodule FontMetrics do
       },
     }}
   end
-  defp intrepret_unpack(%{"version" => version}) when version != version do
+  defp intrepret_unpack(%{"version" => version}) when version != @version do
     {:error, :version, version}
   end
   defp intrepret_unpack(%{"signature_type" => sig}) when sig != @signature_name do
@@ -136,7 +141,7 @@ defmodule FontMetrics do
   defp intrepret_unpack!(map) do
     case intrepret_unpack(map) do
       {:ok, font_metrics} -> font_metrics
-      err -> raise %FontMetrics.Error{error: err, data: map}
+      err -> raise %FontMetrics.Error{message: "Invalid metrics", error: err, data: map}
     end
   end
 
@@ -144,19 +149,19 @@ defmodule FontMetrics do
   # validity checks
 
   #--------------------------------------------------------
-  def is_supported?( codepoint, %FontMetrics{metrics: metrics, version: @version} )
+  def supported?( codepoint, %FontMetrics{metrics: metrics, version: @version} )
   when is_integer(codepoint) do
     Map.has_key?(metrics, codepoint)
   end
 
-  def is_supported?( char_list, %FontMetrics{} = metrics ) when is_list(char_list) do
-    Enum.all?( char_list, &is_supported?(&1, metrics) )
+  def supported?( char_list, %FontMetrics{} = metrics ) when is_list(char_list) do
+    Enum.all?( char_list, &supported?(&1, metrics) )
   end
 
-  def is_supported?( string, %FontMetrics{} = metrics ) when is_bitstring(string) do
+  def supported?( string, %FontMetrics{} = metrics ) when is_bitstring(string) do
     string
     |> String.to_charlist()
-    |> is_supported?( metrics )
+    |> supported?( metrics )
   end
 
   #============================================================================
@@ -164,11 +169,22 @@ defmodule FontMetrics do
 
   #--------------------------------------------------------
   def ascent( pixels, font_metrics )
+  def ascent( nil, %FontMetrics{ ascent: ascent, version: @version } ), do: ascent
   def ascent(
     pixels,
-    %FontMetrics{ ascent: ascent, ascent: ascent, descent: descent, version: @version }
+    %FontMetrics{ ascent: ascent, descent: descent, version: @version }
   ) do
     ascent * (pixels / (ascent - descent))
+  end
+
+  #--------------------------------------------------------
+  def descent( pixels, font_metrics )
+  def descent( nil, %FontMetrics{ descent: descent, version: @version } ), do: descent
+  def descent(
+    pixels,
+    %FontMetrics{ ascent: ascent, descent: descent, version: @version }
+  ) do
+    descent * (pixels / (ascent - descent))
   end
 
   #--------------------------------------------------------
@@ -176,6 +192,7 @@ defmodule FontMetrics do
 
   #--------------------------------------------------------
   def max_box( pixels, font_metrics )
+  def max_box( nil, %FontMetrics{ max_box: max_box, version: @version } ), do: max_box
   def max_box(
     pixels,
     %FontMetrics{
@@ -188,35 +205,19 @@ defmodule FontMetrics do
   end
 
   #--------------------------------------------------------
-  def metrics( source, pixels, font_metrics )
-
-  def metrics(
-    source, pixels,
-    %FontMetrics{metrics: cp_metrics, ascent: ascent, descent: descent, version: @version}
-  ) when is_number(pixels) and pixels > 0 do
-    scale = pixels / (ascent - descent)
-    do_metrics( source, scale, cp_metrics )
-  end
-
-  defp do_metrics( codepoint, scale, cp_metrics ) when is_integer(codepoint) do
-    case cp_metrics[codepoint] do
-      nil -> {:error, :not_found}
-      adv -> adv * scale
-    end
-  end
-
-  defp do_metrics( codepoints, scale, cp_metrics ) when is_list(codepoints) do
-    Enum.map(codepoints, &do_metrics(&1, scale, cp_metrics) )
-  end
-
-  defp do_metrics( string, scale, cp_metrics ) when is_bitstring(string) do
-    string
-    |> String.to_charlist()
-    |> do_metrics( scale, cp_metrics )
-  end
-
-  #--------------------------------------------------------
   def width( source, pixels, font_metrics, kern \\ false )
+
+  def width( "", _, _, _ ), do: 0
+  def width( '', _, _, _ ), do: 0
+
+  def width( source, nil,
+    %FontMetrics{
+      metrics: cp_metrics, kerning: kerning, version: @version
+    },
+    kern
+  ) do
+    do_width( source, 1.0, cp_metrics, kerning, kern )
+  end
 
   def width(
     source, pixels,
@@ -243,15 +244,22 @@ defmodule FontMetrics do
     end) * scale
   end
 
-  # yes kerning. harder.
+  # yes. kerning. harder.
   defp do_width( codepoints, scale, cp_metrics, kerning, true ) when is_list(codepoints) do
     do_kerned_width( codepoints, scale, cp_metrics, kerning )
   end
 
+  # if the string has multiple lines, return the width of the longest one
   defp do_width( string, scale, cp_metrics, kerning, kern ) when is_bitstring(string) do
     string
-    |> String.to_charlist()
-    |> do_width( scale, cp_metrics, kerning, kern )
+    |> String.split( "\n")
+    |> Enum.map( &String.to_charlist(&1) )
+    |> Enum.reduce( 0, fn(line, max_width) ->
+      case do_width( line, scale, cp_metrics, kerning, kern ) do
+        width when width > max_width -> width
+        _ -> max_width
+      end
+    end)
   end
 
   defp do_kerned_width( codepoints, scale, cp_metrics, kerning, width \\ 0 )
@@ -267,7 +275,87 @@ defmodule FontMetrics do
     do_kerned_width( codepoints, scale, cp_metrics, kerning, width )
   end
 
+  #--------------------------------------------------------
+
+
+import IEx
+  def shorten( source, max_width, pixels, font_metrics, opts \\ [] )
+
+  def shorten(
+    source, max_width, pixels,
+    %FontMetrics{
+      metrics: cp_metrics, ascent: ascent, descent: descent,
+      kerning: kerning, version: @version
+    } = font_metrics,
+    opts
+  ) when is_list(source) and is_list(opts) do
+    kern = !!opts[:kern]
+    terminator = case opts[:terminator] do
+      nil -> '...'
+      t when is_list(t) -> t
+      t when is_bitstring(t) -> String.to_charlist(t)
+    end
+
+    # calculate the scale to use
+    scale = case pixels do
+      nil -> 1.0
+      p -> p / (ascent - descent)
+    end
+
+    # terminator_width = do_width( terminator, scale, font_metrics, kern )
+    terminator_width = do_width( terminator, scale, cp_metrics, kerning, kern )
+    do_shorten(
+      source,
+      max_width - terminator_width,
+      scale, font_metrics, kern
+    )
+    |> case do
+      '' -> ''
+      out -> (terminator ++ out) |> Enum.reverse()
+    end
+  end
+
+  def shorten( source, width, max_pixels, %FontMetrics{} = font_metrics, opts )
+  when is_bitstring(source) do
+    source
+    |> String.split( "\n")
+    |> Enum.map( &String.to_charlist(&1) )
+    |> Enum.map( &shorten( &1, width, max_pixels, font_metrics, opts ) )
+    |> Enum.map( &to_string(&1) )
+    |> Enum.join( "\n" )
+  end
+
+  def do_shorten( _, max_width, _, _, _ ) when max_width <= 0, do: ''
+
+  # various ways to structure the code. This attempts to reuse calculates and
+  # and keep it to a single pass as much as possible
+  def do_shorten(
+    source, max_width, scale,
+    %FontMetrics{metrics: cp_metrics},
+    false
+  ) do
+    max_width = max_width / scale
+    {out,_} = Enum.reduce_while(source, {[],0}, fn(cp, {acc,width})->
+      new_width = width + (cp_metrics[cp] || cp_metrics[0])
+      cond do
+        new_width < max_width -> {:cont, { [cp | acc], new_width}}
+        true -> {:halt, {acc,width}}
+      end
+    end)
+    out
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
 
 
 
--- test/font_metrics_test.exs
+++ test/font_metrics_test.exs
@@ -1,5 +1,278 @@
+#
+#  Created by Boyd Multerer on 28/02/19.
+#  Copyright © 2019 Kry10 Industries. All rights reserved.
+#
+
 defmodule FontMetricsTest do
   use ExUnit.Case
   doctest FontMetrics
 
+  import IEx
+
+  @bitter   "test/metrics/bitter.metrics"
+  @bitter_signature "HhK_V3WHLydpJjQ25BxFUv8Q_jn7iHzkezCraBo76gg"
+  @bitter_metrics   File.read!( @bitter ) |> FontMetrics.from_binary!()
+
+
+  @roboto   "test/metrics/roboto.metrics"
+  @roboto_signature "eehRQEZX2sIQaz0irSVtR4JKmldlRY7bcskQKkWBbZU"
+  @roboto_metrics   File.read!( @roboto ) |> FontMetrics.from_binary!()
+
+  @hash_type  :sha256
+
+  @version            "0.1.0"
+
+  #============================================================================
+
+  test "expected_hash" do
+    assert FontMetrics.expected_hash() == @hash_type
+  end
+
+  #============================================================================
+  # from_binary( binary )
+
+  test "from_binary decodes bitter into a proper struct" do
+    bin = File.read!( @bitter )
+
+    {:ok, %FontMetrics{} = metrics} = FontMetrics.from_binary( bin )
+    assert metrics.version == @version
+
+    assert metrics.max_box == {-60, -265, 1125, 935}
+    assert metrics.units_per_em == 1000
+    assert metrics.smallest_ppem == 9
+    assert metrics.direction == 2
+    assert metrics.kerning[{66, 65}] == -30
+
+    assert metrics.source.signature_type == @hash_type
+    assert metrics.source.signature == @bitter_signature
+    assert metrics.source.font_type == "TrueType"
+  end
+
+  test "from_binary decodes roboto into a proper struct" do
+    bin = File.read!( @roboto )
+
+    {:ok, %FontMetrics{} = metrics} = FontMetrics.from_binary( bin )
+    assert metrics.version == @version
+
+    assert metrics.max_box == {-1509, -555, 2352, 2163}
+    assert metrics.units_per_em == 2048
+    assert metrics.smallest_ppem == 9
+    assert metrics.direction == 2
+    assert metrics.kerning == %{}
+
+    assert metrics.source.signature_type == @hash_type
+    assert metrics.source.signature == @roboto_signature
+    assert metrics.source.font_type == "TrueType"
+  end
+
+  test "from_binary returns error if the data is bad" do
+    bin = File.read!( @bitter )
+    assert FontMetrics.from_binary( "garbage" <> bin ) == {:error, :unzip}
+  end
+
+  #============================================================================
+  # from_binary!( binary )
+
+  test "from_binary! decodes bitter into a proper struct" do
+    bin = File.read!( @bitter )
+
+    %FontMetrics{} = metrics = FontMetrics.from_binary!( bin )
+    assert metrics.version == @version
+
+    assert metrics.max_box == {-60, -265, 1125, 935}
+    assert metrics.units_per_em == 1000
+    assert metrics.smallest_ppem == 9
+    assert metrics.direction == 2
+    assert metrics.kerning[{66, 65}] == -30
+
+    assert metrics.source.signature_type == @hash_type
+    assert metrics.source.signature == @bitter_signature
+    assert metrics.source.font_type == "TrueType"
+  end
+
+  test "from_binary! raises if the data is bad" do
+    bin = File.read!( @bitter )
+    assert_raise ErlangError, fn ->
+      FontMetrics.from_binary!( "garbage" <> bin )
+    end
+  end
+
+  #============================================================================
+  # to_binary( binary )
+
+  test "to_binary( binary ) works" do
+    assert FontMetrics.to_binary( @bitter_metrics ) == File.read(@bitter)
+  end
+ 
+  test "to_binary( binary ) enforces the version" do
+    assert_raise FunctionClauseError, fn ->
+      FontMetrics.to_binary( %{@bitter_metrics | version: "not supported"} )
+    end
+  end
+ 
+  test "to_binary!( binary ) works" do
+    assert FontMetrics.to_binary!( @bitter_metrics ) == File.read!(@bitter)
+  end
+  
+  #============================================================================
+  # supported?
+
+  test "supported? checks single characters" do
+    assert FontMetrics.supported?( 32, @roboto_metrics )
+    assert FontMetrics.supported?( 'A', @roboto_metrics )
+    assert FontMetrics.supported?( "A", @roboto_metrics )
+    assert FontMetrics.supported?( 'A', @bitter_metrics )
+
+    refute FontMetrics.supported?( 25324, @roboto_metrics )
+    refute FontMetrics.supported?( '括', @roboto_metrics )
+    refute FontMetrics.supported?( "括", @roboto_metrics )
+    refute FontMetrics.supported?( "括", @bitter_metrics )
+
+    assert FontMetrics.supported?( 'Ж', @roboto_metrics )
+    refute FontMetrics.supported?( 'Ж', @bitter_metrics )
+  end
+
+  test "supported? checks multiple characters" do
+    assert FontMetrics.supported?( "Multiple Characters.", @roboto_metrics )
+    assert FontMetrics.supported?( 'Multiple Characters.', @roboto_metrics )
+
+    refute FontMetrics.supported?( 'Multiple Characters 括', @roboto_metrics )
+  end
+
+  #============================================================================
+  # points_to_pixels( points )
+
+  test "points_to_pixels does a simple conversion" do
+    assert FontMetrics.points_to_pixels( 12 ) == 16.0
+  end
+
+  #============================================================================
+  # ascent(pixels, font_metric)
+
+  test "ascent returns the raw value if pixels is nil" do
+    assert FontMetrics.ascent( nil, @bitter_metrics ) == @bitter_metrics.ascent
+  end
+
+  test "ascent returns a scaled value" do
+    raw_value = FontMetrics.ascent( nil, @roboto_metrics)
+    scale = 22 / (@roboto_metrics.ascent - @roboto_metrics.descent)
+
+    assert FontMetrics.ascent( 22, @roboto_metrics) == raw_value * scale
+  end
+
+  #============================================================================
+  # descent(pixels, font_metric)
+
+  test "descent returns the raw value if pixels is nil" do
+    assert FontMetrics.descent( nil, @roboto_metrics ) == @roboto_metrics.descent
+  end
+
+  test "descent returns a scaled value" do
+    raw_value = FontMetrics.descent( nil, @roboto_metrics)
+    scale = 22 / (@roboto_metrics.ascent - @roboto_metrics.descent)
+
+    assert FontMetrics.descent( 22, @roboto_metrics) == raw_value * scale
+  end
+
+  #============================================================================
+  # max_box(pixels, font_metric)
+
+  test "max_box returns the raw value if pixels is nil" do
+    assert FontMetrics.max_box( nil, @roboto_metrics ) == @roboto_metrics.max_box
+  end
+
+  test "max_box returns a scaled value" do
+    {xv_min, yv_min, xv_max, yv_max} = FontMetrics.max_box( nil, @roboto_metrics)
+
+    scale = 22 / (@roboto_metrics.ascent - @roboto_metrics.descent)
+    {x_min, y_min, x_max, y_max} = FontMetrics.max_box( 22, @roboto_metrics)
+
+    assert x_min == xv_min * scale
+    assert y_min == yv_min * scale
+    assert x_max == xv_max * scale
+    assert y_max == yv_max * scale
+  end
+
+
+  #============================================================================
+  # width( source, pixels, font_metric)
+
+  test "width of an empty string is always 0" do
+    assert FontMetrics.width( "", nil, @roboto_metrics) == 0
+    assert FontMetrics.width( "", nil, @bitter_metrics, true) == 0
+    assert FontMetrics.width( "", 24, @roboto_metrics) == 0
+  end
+
+  test "width returns the raw width of the source if pixels is nil" do
+    raw_value = @roboto_metrics.metrics[97] + 
+      @roboto_metrics.metrics[98] + @roboto_metrics.metrics[99]
+
+    assert FontMetrics.width( "abc", nil, @roboto_metrics) == raw_value
+    assert FontMetrics.width( 'abc', nil, @roboto_metrics) == raw_value
+    assert FontMetrics.width( 97, nil, @roboto_metrics) == @roboto_metrics.metrics[97]
+  end
+
+  test "width returns the scaled width of the source according to pixel height" do
+    metrics = @roboto_metrics
+
+    raw_value = metrics.metrics[97] + metrics.metrics[98] + metrics.metrics[99]
+    scale = 22 / (metrics.ascent - metrics.descent)
+
+    assert FontMetrics.width( "abc", 22, metrics) == raw_value * scale
+    assert FontMetrics.width( 'abc', 22, metrics) == raw_value * scale
+    assert FontMetrics.width( 97, 22, metrics) == metrics.metrics[97] * scale
+  end
+
+  test "width accounts for kerning if the options is set" do
+    string = "PANCAKE"
+    metrics = @bitter_metrics
+
+    raw_width = FontMetrics.width( string, nil, metrics )
+    raw_kerned = FontMetrics.width( string, nil, metrics, true )
+    assert raw_kerned < raw_width
+
+    scale = 22 / (metrics.ascent - metrics.descent)
+    assert FontMetrics.width( string, 22, metrics ) == raw_width * scale
+    assert FontMetrics.width( string, 22, metrics, true ) == raw_kerned * scale
+  end
+
+  test "width the returns the longest line of a multiline string" do
+    longest = "the middle part"
+    string = "first part\nthe middle part\nlast part"
+    assert FontMetrics.width( string, 22, @roboto_metrics ) ==
+      FontMetrics.width( longest, 22, @roboto_metrics )
+  end
+
+
+  #============================================================================
+  # shorten( source, max_width, pixels, font_metric, terminator \\ "...")
+
+  test "shorten shortens a string with an ..." do
+    string = "This string will be shortened to the requested width"
+    assert FontMetrics.shorten( string, 216, 22, @roboto_metrics ) == 
+      "This string will be short..."
+  end
+
+  test "shorten shortens a string with custom terminator" do
+    string = "This string will be shortened to the requested width"
+    assert FontMetrics.shorten( string, 226, 22, @roboto_metrics, terminator: "___" ) ==
+      "This string will be short___"
+  end
+
+  test "shorten returns an empty string if the max width is too small for the terminator" do
+    string = "This string will be shortened to the requested width"
+    assert FontMetrics.shorten( string, 2, 22, @roboto_metrics ) == ""
+  end
+
+  test "shorten works with lines in a multiline string" do
+    string = "This string\nwill be shortened to the requested\nwidth"
+    assert FontMetrics.shorten( string, 100, 22, @roboto_metrics ) ==
+      "This string\nwill be shortened...\nwidth"
+  end
+
 end
+
+
+
+
+
