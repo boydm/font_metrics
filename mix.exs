42a0bc8a507e3401b87afd3f7a855e28c4fd6180
Boyd Multerer <boyd@multerer.com>
2021-05-21 10:00:33 +1200

    make sha256 the default again

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule FontMetrics.MixProject do
 
   @app_name :font_metrics
 
-  @version "0.5.0"
+  @version "0.5.1"
 
   @elixir_version "~> 1.7"
   @github "https://github.com/boydm/font_metrics"
